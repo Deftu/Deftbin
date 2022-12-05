24f88194ae795522068683151b83b5568ff8d74a
Deftu <deftudev@gmail.com>
2022-12-05 23:54:56 +0200

    Make database connections retry on close and handle errors.
    Signed-off-by: Deftu <deftudev@gmail.com>

--- backend/src/database/document.ts
+++ backend/src/database/document.ts
@@ -4,15 +4,38 @@ import mongoose, {
 import config from "$config";
 let documentConnection: Connection;
 
-try {
-    documentConnection = mongoose.createConnection(`mongodb://${config.database.host}${config.database.port ? `:${config.database.port}` : ""}`, {
-        dbName: "documents"
-    });
+const connectionOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "documents",
+    connectTimeoutMS: 5000,
+    socketTimeoutMS: 5000,
+    serverSelectionTimeoutMS: 5000,
+};
 
-    console.log("Connected to document database.");
-} catch (err) {
+documentConnection = mongoose.createConnection(`mongodb://${config.database.host}${config.database.port ? `:${config.database.port}` : ""}`, connectionOptions);
+
+documentConnection.on("error", (err) => {
     console.error(err);
-    throw new Error("Could not connect to document database.");
-}
+    throw err;
+});
+
+documentConnection.on("open", () => {
+    console.log("Connected to document database");
+});
+
+documentConnection.on("close", () => {
+    console.log("Document database connection closed. Retrying to connect...");
+    let retries = 0;
+    const retryInterval = setInterval(() => {
+        if (retries >= 5) {
+            clearInterval(retryInterval);
+            throw new Error("Could not connect to document database");
+        }
+
+        retries++;
+        documentConnection.openUri(`mongodb://${config.database.host}${config.database.port ? `:${config.database.port}` : ""}`, connectionOptions);
+    }, 1000);
+});
 
 export default documentConnection;
